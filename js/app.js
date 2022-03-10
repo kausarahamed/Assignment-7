8138f1beaecfc7670e4b49c428bcdcc6f9943a7b
kausarahamed <galibhasan517@gmail.com>
2022-03-10 23:47:24 +0600

    All problem solve

--- js/app.js
+++ js/app.js
@@ -143,6 +143,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById("liked").innerHTML="";
   const likedPosts = getLikedPosts();
   likedPosts.forEach((post) => {
     const div = createPost(post);
@@ -151,6 +152,7 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+  document.getElementById("reported").innerHTML="";
   const reportedPosts = getReportedPosts();
   reportedPosts.forEach((post) => {
     const div = createPost(post);
